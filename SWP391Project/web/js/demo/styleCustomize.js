9bc3034670cbb33e5b8be514f0d51ccb82d3b6d1
truong-magnus <122264583+TruongCNSE161225@users.noreply.github.com>
2023-03-31 18:33:17 +0700

    update

--- SWP391Project/web/js/demo/styleCustomize.js
+++
@@ -1,8 +0,0 @@
-
-
-function showCalendarMonth(){
-    const calendarSearch = document.querySelector(".calendar_search");
-    const calendarButton = document.querySelector(".fa-calendar");
-    calendarButton.classList.toggle("text-gray-600")
-        calendarSearch.classList.toggle("active");
-}
